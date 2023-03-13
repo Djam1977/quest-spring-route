dddf071b9b3a62d24efe5e7f3480d7f729a299de
Djam1977 <117649014+Djam1977@users.noreply.github.com>
2023-03-13 16:02:34 +0100

    quete2

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -3,15 +3,17 @@ package com.wildcodeschool.doctor.controller;
 import com.wildcodeschool.doctor.model.Doctor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
+    public Doctor doctor(@PathVariable int number) {
 
-        return null;
+
+        return new Doctor(  13,  "Jodie Whittaker");
     }
 }
